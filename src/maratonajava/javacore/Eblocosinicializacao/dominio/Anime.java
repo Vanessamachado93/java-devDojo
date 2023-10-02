aaa316225165a76781e4cc2a1dca7b9fb8544842
Vanessamachado93 <machado.vanessa32@gmail.com>
2023-10-02 11:50:47 -0300

    Maratona Java Virado no Jiraya - Aulas de 60 - OrBloco de Inicializacao

--- src/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
+++ src/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -1,2 +1,36 @@
-package maratonajava.javacore.Eblocosinicializacao.dominio;public class Anime {
+package maratonajava.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    //1 - Alocado espaço em memória pro objeto
+    //2 - Cada atributo de classe é criando e inicializado com valores default ou o que for passado
+    //3 - Bloco de inicialização é executado
+    //4- Construtor é executado
+    {
+        System.out.println("Dentro do Bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for (int episodio: this.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println( );
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
 }
---
+++ src/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -0,0 +1,15 @@
+package maratonajava.javacore.Eblocosinicializacao.test;
+
+import maratonajava.javacore.Eblocosinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One Piece");
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+            
+        }
+
+    }
+}
