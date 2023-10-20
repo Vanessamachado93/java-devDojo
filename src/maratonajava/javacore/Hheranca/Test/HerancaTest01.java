652a7d1a594148c570271c4227749cc10bbd41f6
Vanessamachado93 <machado.vanessa32@gmail.com>
2023-10-20 20:27:15 -0300

    Maratona Java Virado no Jiraya - Aulas de 72 - Herança

---
+++ src/maratonajava/javacore/Hheranca/Test/HerancaTest01.java
@@ -0,0 +1,27 @@
+package maratonajava.javacore.Hheranca.Test;
+
+import maratonajava.javacore.Hheranca.dominio.Endereco;
+import maratonajava.javacore.Hheranca.dominio.Funcionario;
+import maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("96700-000");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Naruto");
+        pessoa.setCpf("111111111");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunda");
+        funcionario.setCpf("22222222");
+        System.out.println("----------");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(2000);
+        funcionario.imprime();
+    }
+}
---
+++ src/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -0,0 +1,16 @@
+package maratonajava.javacore.Hheranca.dominio;
+
+public class Funcionario extends Pessoa{
+    private double salario;
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+
+
