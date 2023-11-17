db98dd5d154de0f86c3f84d132ced264cc5a1ac7
vanessamachado93 <machado.vanessa32@gmail.com>
2023-11-17 12:08:06 -0300

    Maratona Java Virado no Jiraya - Tratamento de Erros - Exceções

---
+++ src/maratonajava/javacore/Oexeception/exception/test/ExceptionTest01.java
@@ -0,0 +1,21 @@
+package maratonajava.javacore.Oexeception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+
+    }
+
+    private static void criarNovoArquivo(){
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado "+isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
