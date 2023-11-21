8cacbd860284dd667662902d273f038310559b79
vanessamachado93 <machado.vanessa32@gmail.com>
2023-11-21 11:10:39 -0300

    Maratona Java Virado no Jiraya - Tratamento de ErrxceÃceções - Try with resources

--- src/maratonajava/javacore/Oexeception/exception/test/RuntimeExceptionTest05.java
+++ src/maratonajava/javacore/Oexeception/exception/test/RuntimeExceptionTest05.java
@@ -1,19 +1,17 @@
 package maratonajava.javacore.Oexeception.exception.test;
 
+import javax.sound.midi.Soundbank;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 
-public class RuntimeExceptionTest04 {
+public class RuntimeExceptionTest05 {
     public static void main(String[] args) {
-        try{
+
+        try {
             throw new RuntimeException();
-        }catch (ArrayIndexOutOfBoundsException e){
-        System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        }catch (IndexOutOfBoundsException e){
-        System.out.println("Dentro do IndexOutOfBoundsException");
-        }catch(IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        }catch (RuntimeException e){
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e){
+            System.out.println("Dentro do ArrayIndex | IllegalArgumention | ArithmeticException");
+        } catch (RuntimeException e){
             System.out.println("Dentro do RuntimeException");
         }
 
