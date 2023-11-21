448d7e44d8c0d9c1c1947c9967f719a80dad8b54
vanessamachado93 <machado.vanessa32@gmail.com>
2023-11-21 09:17:10 -0300

    Maratona Java Virado no Jiraya - Tratamento de Errxceções - Bloco Finally

---
+++ src/maratonajava/javacore/Oexeception/exception/test/RuntimeExceptionTest03.java
@@ -0,0 +1,21 @@
+package maratonajava.javacore.Oexeception.exception.test;
+
+public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
+        try{
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "Conexão aberta";
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("Fechando recurso liberado pelo sistema operacional");
+        }
+        return null;
+    }
+}
